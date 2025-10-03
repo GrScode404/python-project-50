be7dec59fea6a35517ae806082026bc5b446b4b4
GrScode404 <grigoryev.nhpt@gmail.com>
2025-10-03 11:18:22 +0300

    fixed gendiff.py .gitignore

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,11 @@
 import argparse
 
+import json
+
+
+def read_json(file_path):
+    with open(file_path) as f:
+        return json.load(f)
 
 def main():
     parser = argparse.ArgumentParser(description="Compares two configuration"
@@ -7,14 +13,18 @@ def main():
 
     parser.add_argument("first_file")
     parser.add_argument("second_file")
+    parser.add_argument("-f", "--format",
+                        help="set format of output")
 
     args = parser.parse_args()
 
+    first_data = read_json(args.first_file)
+    second_data = read_json(args.second_file)
+
     # Пока просто для проверки:
-    print(args.first_file, args.second_file)
+    print("Первый файл", first_data)
+    print("Второй файл", second_data)
 
-
-    parser.add_argument()
 
 if __name__ == "__main__":
     main()
